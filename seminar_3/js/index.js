e032b411b2af17cd6db5c0b988b78f0ddb4dafae
Barbara Peres <barbarabelyaeva13@gmail.com>
2023-08-16 11:46:35 +0300

    added tasks seminar 2

---
+++ seminar_3/js/index.js
@@ -0,0 +1,15 @@
+
+let varse;
+function Fire(a, b){
+    // varse = parseInt(prompt("Введите число"))
+    return a * b
+}
+
+console.log(Fire(10, 5))
+
+document.querySelector("h1").style.color = "#FF1493"
+//document.getElementById("bg").style.color = "Red"
+
+let nuevo = document.getElementsByClassName("bgn")
+nuevo[0].style.backgroundColor = "#98FB98"
+console.log(nuevo)
